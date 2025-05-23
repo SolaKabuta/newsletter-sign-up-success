842ebe19d9541d7e49f8a1353e222127242cc0dc
Sola Kabuta <solakabuta@gmail.com>
2025-05-23 09:20:47 +0200

    feat: logic and regex added to very the user email.

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 console.log("Yeaah")
-document.getElementById("btn").addEventListener("click", function(e) => {
+
+// We store the email value input
+const mailInput = document.getElementById('mail-input').value;
+
+//
+const mailLabel = document.getElementById('mail-label');
+
+// We store the sucess message
+const sucess = document.getElementById('success-message');
+
+// Email Regex (not quite sure about the regex rule here..)
+function verifyEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return emailRegex.test(email);
+}
+
+// Then we add the event listener on the first button
+document.getElementById("subscribe").addEventListener("click", function(e) {
+  setTimeout(handler)
+ e.preventDefault()
+  const mail = mailInput.trim(); 
+ if (verifyEmail(mail) && mailInput !== "" && mailInput !== " ") {
+   console.log(`Yaaaay it worked!`);
+   
+ } else {
+   alert("It didn't work sorry");
+   
+   // We create the error message
+   const error = document.createElement('p');
+   error.classList.add('text-red', 'font-bold', 'text-xs');
+   error.textContent = "Valid email required";
+   
+   // We append the error next to mail label
+   mailLabel.append(error)
+   
+   mailInput.classList.remove('border-gray-400/90');
+   mailInput.classList.add('text-red');
+   mailInput.classList.add('border-red', 'bg-red');
+ } 
   
 });
 
