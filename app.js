98681cbaca7cb722fee1b282c5bf99ea55d3b072
Sola Kabuta <solakabuta@gmail.com>
2025-05-23 11:15:19 +0200

    test: deploy on Vercel.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 console.log("Yeaah")
 
-// We store the email value input
-const mailInput = document.getElementById('mail-input').value;
-
 //
+const form = document.getElementById('form');
+
+// We store the email value input
+const mailInput = document.getElementById('mail-input').value.trim();
+
+// 
 const mailLabel = document.getElementById('mail-label');
 
 // We store the sucess message
@@ -11,20 +14,23 @@ const sucess = document.getElementById('success-message');
 
 // Email Regex (not quite sure about the regex rule here..)
 function verifyEmail(email) {
-    const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return emailRegex.test(email);
 }
 
+// CLICK EVENT
 // Then we add the event listener on the first button
-document.getElementById("subscribe").addEventListener("click", function(e) {
-  setTimeout(handler)
- e.preventDefault()
+form.addEventListener("submit", function(e) {
+  e.preventDefault()
+  
   const mail = mailInput.trim(); 
- if (verifyEmail(mail) && mailInput !== "" && mailInput !== " ") {
-   console.log(`Yaaaay it worked!`);
-   
- } else {
-   alert("It didn't work sorry");
+  
+  if (!verifyEmail(mail)) {
+    alert("invalid mail")
+    return;
+  } 
+  
+  if (verifyEmail(mail)) {
    
    // We create the error message
    const error = document.createElement('p');
@@ -35,10 +41,20 @@ document.getElementById("subscribe").addEventListener("click", function(e) {
    mailLabel.append(error)
    
    mailInput.classList.remove('border-gray-400/90');
-   mailInput.classList.add('text-red');
-   mailInput.classList.add('border-red', 'bg-red');
+   mailInput.classList.add('bg-red-200', 'text-red-600', 'border-red-500');
+  return;
  } 
-  
+ 
+  console.log(`Yaaaay it worked!`);
+  setTimeout(() => {
+    form.submit();
+    // We hide the form
+    document.getElementById('form').classList.add('hidden')
+    
+    // And then display the success message
+    success.classList.remove('hidden');
+    success.classList.add('block');
+  },1700) 
 });
 
 
