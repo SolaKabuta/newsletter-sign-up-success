f61081fb63363c8dd1debe1538b0722a4cc858c4
Sola Kabuta <solakabuta@gmail.com>
2025-05-23 12:09:19 +0200

    feat: javascript logic added to submit the form and then refresh the page witht the last button

--- app.js
+++ app.js
@@ -1,59 +1,70 @@
-console.log("Yeaah")
+console.log("Yeaah");
 
-//
+// Inputs 
 const form = document.getElementById('form');
-
-// We store the email value input
-const mailInput = document.getElementById('mail-input').value.trim();
-
-// 
+const mailInput = document.getElementById('mail-input');
 const mailLabel = document.getElementById('mail-label');
+const success = document.getElementById('success-message');
 
-// We store the sucess message
-const sucess = document.getElementById('success-message');
-
-// Email Regex (not quite sure about the regex rule here..)
+// Email verification 
 function verifyEmail(email) {
-    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return emailRegex.test(email);
+  const emailRegex = /^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return emailRegex.test(email);
 }
 
-// Click Event
-// Then we add the event listener on the first button
-form.addEventListener("submit", function(e) {
-  e.preventDefault()
-  
-  const mail = mailInput.trim(); 
-  
-  if (!verifyEmail(mail)) {
-    alert("invalid mail")
+// Submit event
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = mailInput.value.trim();
+
+  // We delete the error
+  const existingError = mailLabel.querySelector('p');
+  if (existingError) {
+    existingError.remove();
+  }
+
+  // Email verification
+  if (!verifyEmail(email)) {
+    // Error Message
+    const error = document.createElement('p');
+    error.classList.add('text-red', 'font-bold', 'text-xs');
+    error.textContent = "Valid email required";
+
+    // Add a message next to mail label
+    mailLabel.appendChild(error);
+
+    // Add class to the error message
+    mailInput.classList.remove('border-gray-400/90');
+    mailInput.classList.add('bg-red-200', 'text-red-600', 'border-red-500');
+    mailInput.value.textContent = " ";
     return;
-  } 
-  
-  if (verifyEmail(mail)) {
-   // We create the error message
-   const error = document.createElement('p');
-   error.classList.add('text-red', 'font-bold', 'text-xs');
-   error.textContent = "Valid email required";
-   
-   // We append the error next to mail label
-   mailLabel.append(error)
-   
-   mailInput.classList.remove('border-gray-400/90');
-   mailInput.classList.add('bg-red-200', 'text-red-600', 'border-red-500');
-  return;
- } 
- 
+  }
+
+  // Styles reset
+  mailInput.classList.remove('bg-red-200', 'text-red-600', 'border-red-500');
+  mailInput.classList.add('border-gray-400/90');
+
   console.log(`Yaaaay it worked!`);
+
+  // Success message display
   setTimeout(() => {
-    form.submit();
-    // We hide the form
-    document.getElementById('form').classList.add('hidden')
-    
-    // And then display the success message
+    form.classList.add('hidden');
     success.classList.remove('hidden');
     success.classList.add('block');
-  },1700) 
+  }, 700);
+  
+  
 });
 
+// Dismiss button event
+let dismiss = document.getElementById("dismiss")
 
+dismiss.addEventListener("click", function(e) {
+  e.preventDefault()
+    success.classList.add("hidden")
+    setTimeout(() =>{
+      // reload the page after 1s
+      location.reload();
+    },1000)
+})
